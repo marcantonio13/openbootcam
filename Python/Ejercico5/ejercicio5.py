c7f3e085048b0b2a4253abedc4b268dd663c7248
marcantonio13 <107515956+marcantonio13@users.noreply.github.com>
2022-10-12 16:17:28 -0500

    Subir ejercicio5 una función que devulva un año bisisesto

---
+++ Python/Ejercico5/ejercicio5.py
@@ -0,0 +1,19 @@
+
+# Escribe una función que pueda decirte si un año (número entero) es bisiesto o no.
+
+
+def bisiesto(x):  
+  if((x % 400 == 0) or  
+     (x % 100 != 0) and  
+     (x % 4 == 0)):   
+    print("El año es bisiesto!");    
+  else:  
+    print ("El año no es  Bisiesto!")    
+x = int(input("Dame el año: "))   
+
+
+bisiesto(x)  
+
+
+
+
