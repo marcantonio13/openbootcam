72a24fbe28355d8a8fb5fea532d83b53366a0712
marcantonio13 <107515956+marcantonio13@users.noreply.github.com>
2022-11-07 17:45:37 -0600

    tema2 ejercicio 1 java basico

---
+++ Java_basico/tema2/src/com/openBootcamp/Metodos.java
@@ -0,0 +1,31 @@
+package com.openBootcamp;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		
+		
+		// numeros  
+		
+		int numero1 = 3;
+		float numero2 = 3; 
+		double numero3 = 4.5; 
+		long numero4 = 45433; 
+		short numero5 = 5; 
+		byte numero6 = 9;
+		
+		
+		
+		boolean variable1 = true; 
+		boolean varible2 = false; 
+		
+		char varible3 = 'q'; 
+		
+		String varible4 = "Hola mundo";
+	
+		
+
+
+	}
+
+}
